ee5629df3d4c437e9647eb194b39ca0db9689925
suraj rawat <surajrwat001@gmail.com>
2025-04-05 08:02:40 +0530

    Initial Commit

---
+++ server/models/transactionModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    plan: { type: String, required: true },
+    amount: { type: Number, required: true },
+    credits: { type: Number, required: true },
+    payment: { type: Boolean, default: false },
+    date: { tpe: Number }
+})
+
+const transactionModel = mongoose.models.transaction || mongoose.model("transaction", transactionSchema)
+export default transactionModel
---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
+
+const PORT = process.env.PORT || 4000
+const app = express()
+
+app.use(express.json()); // Add this middleware to parse JSON body
+app.use(express.urlencoded({ extended: true })); // If you're sending form data
+// app.use(cors())
+app.use(cors({ origin: "http://localhost:5173" }));
+await connectDB()
+
+app.use('/api/user', userRouter)
+app.use('/api/image', imageRouter)
+app.get('/', (req, res) => {
+    res.send("API working")
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+
+// user - suraj@gmail.com, 123456
